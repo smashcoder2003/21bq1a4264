da88ea0fcba06ad88581b052b96df493deabb061
smashcoder2003 <106431623+smashcoder2003@users.noreply.github.com>
2022-10-12 21:32:48 +0530

    Update Exp3.py

--- Python/AdvancedPython/LabCycle/Exp3.py
+++ Python/AdvancedPython/LabCycle/Exp3.py
@@ -2,5 +2,5 @@ names = {1: "Rama",
          2: "Ravi",
          3: "Raju"}
 position = 2
-result = {element[0]: element[1] for element in sorted(names.items(), key=lambda x: x[position-1][2])}
+result = {element[0]: element[1] for element in sorted(names.items(), key=lambda x: x[1][position])}
 print(result)
